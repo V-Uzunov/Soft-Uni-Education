ea04644ae935120975c0b779d3785f848b8a50a6
V-Uzunov <vega_99@abv.bg>
2018-02-07 17:02:22 +0200

    Exercise added

--- 11.JSFundamentals/06.StringsAndRegEx/02.Exercises/10.SecretData.js
+++ 11.JSFundamentals/06.StringsAndRegEx/02.Exercises/10.SecretData.js
@@ -14,4 +14,16 @@ function secretDataRegex(input) {
     }
 }
 
-secretDataRegex(['Agent *Ivankov was in the room when it all happened. The person in the room was heavily armed. Agent * Ivankov had to act quick in order. He picked up his phone and called some unknown number.  I think it was + 555 - 49 - 796 I can't really remember... He said something about "finishing work" with subject!2491a23BVB34Q andreturning to Base _Aurora21Then after that he disappeared from my sight.As if he vanished in the shadows.A moment, I really don't know what happened there.This is all I saw, that night.I cannot explain it myself...']);
+secretDataRegex(['Agent *Ivankov was in the room when it all happened.',
+    'The person in the room was heavily armed.',
+    'Agent *Ivankov had to act quick in order.',
+    'He picked up his phone and called some unknown number. ',
+    'I think it was +555-49-796...',
+    'I can\'t really remember...',
+    'He said something about "finishing work with subject !2491a23BVB34Q and returning to Base _Aurora21".',
+    'Then after that he disappeared from my sight.',
+    'As if he vanished in the shadows.',
+    'A moment, shorter than a second, later, I saw the person flying off the top floor.',
+    'I really don\'t know what happened there.',
+    'This is all I saw, that night.',
+    'I cannot explain it myself...']);
