ded05c58791f670efacbe83791a28c341ff60f9f
V-Uzunov <vega_99@abv.bg>
2018-03-07 09:21:35 +0200

    JS Prototype and Chain Lab Added

---
+++ 12.JSAdvanced/08.PrototypeChain/02.InheritingtoString.js
@@ -0,0 +1,38 @@
+function personAndTeacher() {
+    class Person{
+        constructor(name, email){
+            this.name = name;
+            this.email = email;
+        }
+        toString(){
+            let className = this.constructor.name;
+            return `${className} (name: ${this.name}, email: ${this.email})`;
+        }
+    }
+    class Teacher extends Person{
+        constructor (name, email, subject) {
+            super(name, email);
+            this.subject = subject;
+        }
+        toString(){
+            let base = super.toString().slice(0, -1);
+            return base + `, subject: ${this.subject})`;
+        }
+    }
+    class Student extends Person{
+        constructor(name, email, course){
+            super(name, email);
+            this.course = course;
+        }
+        toString(){
+            let base = super.toString().slice(0, -1);
+            return base + `, course: ${this.course})`;
+        }
+    }
+
+    return {
+        Person,
+        Teacher,
+        Student
+    };
+}
---
+++ 12.JSAdvanced/08.PrototypeChain/04.ClassHierarchy.js
@@ -0,0 +1,44 @@
+function solve() {
+    class Figure{
+        constructor(){
+            if (new.target === Figure) {
+                throw new TypeError('Cannot make instanc of abstract class Figure!');
+              }
+        }
+        toString(){
+            let type = this.constructor.name;
+            return type;
+        }
+    }
+    class Circle extends Figure{
+        constructor(radius){
+            super();
+            this.radius = radius;
+        }
+        get area(){
+            return Math.PI * this.radius * this.radius;
+        }
+        toString(){
+            return `${super.toString()} - radius: ${this.radius}`;
+        }
+    }
+    class Rectangle extends Figure{
+        constructor(width, height){
+            super();
+            this.width = width;
+            this.height = height;
+        }
+        get area(){
+            return this.width * this.height;
+        }
+        toString(){
+            return `${super.toString()} - width: ${this.width}, height: ${this.height}`;
+        }
+    }
+
+    return{
+        Figure,
+        Circle,
+        Rectangle
+    };
+}
