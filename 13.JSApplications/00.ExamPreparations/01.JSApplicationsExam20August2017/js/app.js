3b723a77b109609f95acb89e225dc463dd35fb23
V-Uzunov <vega_99@abv.bg>
2018-04-12 12:56:42 +0300

    SeentIt added

--- 13.JSApplications/00.ExamPreparations/01.JSApplicationsExam20August2017/js/app.js
+++ 13.JSApplications/00.ExamPreparations/01.JSApplicationsExam20August2017/js/app.js
@@ -26,7 +26,6 @@ $(() => {
                     .catch(notify.handleError);
             }
         });
-
         this.post('#/login', (ctx) => {
             let username = ctx.params.username;
             let password = ctx.params.password;
@@ -83,8 +82,196 @@ $(() => {
                 .catch(notify.handleError);
         });
 
-        
+        this.get('#/create/post', (ctx) => {
+            if (!auth.isAuth) {
+                ctx.redirect('#/home');
+                return;
+            }else{
+                ctx.loadPartials({
+                    header: './templates/common/header.hbs',
+                    footer: './templates/common/footer.hbs',
+                    navigation: './templates/common/navigation.hbs'
+                }).then(function (){
+                    this.partial('/templates/posts/createPostPage.hbs');
+                });
+            }
+        });
+        this.post('#/create/post', (ctx) => {
+            if (!auth.isAuth) {
+                ctx.redirect('#/home');
+                return;
+            }
+            let url = ctx.params.url;
+            let title = ctx.params.title;
+            let imageUrl = ctx.params.imageUrl;
+            let description = ctx.params.description;
+            let author = sessionStorage.getItem('username');
 
+            if (url === '' || !url.startsWith('http')) {
+                notify.showError('Url cannot be empty and must starts with "http"!');
+            }else if(title === ''){
+                notify.showError('Title cannot be empty!');
+            }else {
+                posts.createPost(author, title, description, url, imageUrl)
+                    .then(() => {
+                        notify.showInfo('Post was created successful!');
+                        ctx.redirect('#/catalog');
+                    }).catch(notify.handleError);
+            }
+        });
+
+        this.get('#/edit/post/:postId', (ctx) => {
+            if (!auth.isAuth()) {
+                ctx.redirect('#/home');
+                return;
+            }
+            let postId = ctx.params.postId;
+
+            posts.getPostById(postId)
+                .then((post) => {
+                    ctx.isAuth = auth.isAuth();
+                    ctx.username = sessionStorage.getItem('username');
+                    ctx.post = post;
+
+                    ctx.loadPartials({
+                        header: './templates/common/header.hbs',
+                        footer: './templates/common/footer.hbs',
+                        navigation: './templates/common/navigation.hbs',
+                    }).then(function () {
+                        this.partial('./templates/posts/editPostPage.hbs');
+                    });
+                });
+        });
+        this.post('#/edit/post', (ctx) => {
+            if (!auth.isAuth) {
+                ctx.redirect('#/home');
+                return;
+            }
+            let postId = ctx.params.postId;
+            let url = ctx.params.url;
+            let title = ctx.params.title;
+            let imageUrl = ctx.params.imageUrl;
+            let description = ctx.params.description;
+            let author = sessionStorage.getItem('username');
+
+            if (url === '' || !url.startsWith('http')) {
+                notify.showError('Url cannot be empty and must starts with "http"!');
+            }else if(title === ''){
+                notify.showError('Title cannot be empty!');
+            }else {
+                posts.editPost(postId, author, title, description, url, imageUrl)
+                    .then(() => {
+                        notify.showInfo(`${title} was updated successful!`);
+                        ctx.redirect('#/catalog');
+                    }).catch(notify.handleError);
+            }
+        });
+        
+        this.get('#/delete/post/:postId', (ctx)=>{
+            if (!auth.isAuth) {
+                ctx.redirect('#/home');
+                return;
+            }
+            let postId = ctx.params.postId;
+
+            posts.deletePost(postId)
+                .then(() => {
+                    notify.showInfo('Post deleted.');
+                    ctx.redirect('#/catalog');
+                })
+                .catch(notify.handleError);
+        });
+        this.get('#/posts', (ctx) => {
+            if (!auth.isAuth()) {
+                ctx.redirect('#/home');
+                return;
+            }
+
+            posts.getMyPosts(sessionStorage.getItem('username'))
+                .then((posts) => {
+                    posts.forEach((p, i) => {
+                        p.rank = i + 1;
+                        p.date = calcTime(p._kmd.ect);
+                        p.isAuthor = p._acl.creator === sessionStorage.getItem('userId');
+                    });
+
+                    ctx.isAuth = auth.isAuth();
+                    ctx.username = sessionStorage.getItem('username');
+                    ctx.posts = posts;
+
+                    ctx.loadPartials({
+                        header: './templates/common/header.hbs',
+                        footer: './templates/common/footer.hbs',
+                        navigation: './templates/common/navigation.hbs',
+                        postList: './templates/posts/postList.hbs',
+                        post: './templates/posts/post.hbs'
+                    }).then(function () {
+                        this.partial('./templates/posts/myPostsPage.hbs');
+                    });
+                })
+        });
+
+        this.get('#/details/:postId', (ctx) => {
+            let postId = ctx.params.postId;
+
+            const postPromise = posts.getPostById(postId);
+            const allCommentsPromise = comments.getPostComments(postId);
+
+            Promise.all([postPromise, allCommentsPromise])
+                .then(([post, comments]) => {
+                    post.date = calcTime(post._kmd.ect);
+                    post.isAuthor = post._acl.creator === sessionStorage.getItem('userId');
+                    comments.forEach((c) => {
+                        c.date = calcTime(c._kmd.ect);
+                        c.commentAuthor = c._acl.creator === sessionStorage.getItem('userId');
+                    });
+
+                    ctx.isAuth = auth.isAuth();
+                    ctx.username = sessionStorage.getItem('username');
+                    ctx.post = post;
+                    ctx.comments = comments;
+
+                    ctx.loadPartials({
+                        header: './templates/common/header.hbs',
+                        footer: './templates/common/footer.hbs',
+                        navigation: './templates/common/navigation.hbs',
+                        postDetails: './templates/details/postDetails.hbs',
+                        comment: './templates/details/comment.hbs'
+                    }).then(function () {
+                        this.partial('./templates/details/postDetailsPage.hbs');
+                    })
+                })
+                .catch(notify.handleError);
+        });
+        this.post('#/create/comment', (ctx) => {
+            let author = sessionStorage.getItem('username');
+            let content = ctx.params.content;
+            let postId = ctx.params.postId;
+
+            if (content === '') {
+                notify.showError('Cannot add empty comment!');
+                return;
+            }
+
+            comments.createComment(postId, content, author)
+                .then(() => {
+                    notify.showInfo('Comment created!');
+                    ctx.redirect(`#/details/${postId}`);
+                })
+                .catch(notify.showError);
+        });
+
+        this.get('#/comment/delete/:commentId/post/:postId', (ctx) => {
+            let commentId = ctx.params.commentId;
+            let postId = ctx.params.postId;
+
+            comments.deleteComment(commentId)
+                .then(() => {
+                    notify.showInfo('Comment deleted.');
+                    ctx.redirect(`#/details/${postId}`);
+                })
+                .catch(notify.handleError);
+        });
         function getWelcomePage(ctx) {
             if (!auth.isAuth()) {
                 ctx.loadPartials({
@@ -94,15 +281,14 @@ $(() => {
                     registerForm: './templates/forms/registerForm.hbs',
                 }).then(function () {
                     this.partial('./templates/welcome-anonymous.hbs');
-                })
+                });
             } else {
                 ctx.redirect('#/catalog');
             }
 
         }
-       
         function calcTime(dateIsoFormat) {
-            let diff = new Date - (new Date(dateIsoFormat));
+            let diff = new Date() - (new Date(dateIsoFormat));
             diff = Math.floor(diff / 60000);
             if (diff < 1) return 'less than a minute';
             if (diff < 60) return diff + ' minute' + pluralize(diff);
